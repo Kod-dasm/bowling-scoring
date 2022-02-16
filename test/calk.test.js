4d2b60e99d5c68fa171dfdaff363749d3e08c619
Kod-dasm <54396012+Kod-dasm@users.noreply.github.com>
2022-02-16 18:48:53 +0500

    Add test for calculateScore(X

--- test/calk.test.js
+++ test/calk.test.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const calk_1 = require("../src/calk");
-describe("test add function", () => {
-    it("should return 15 for add(10,5)", () => {
-        expect((0, calk_1.add)(10, 5)).toBe(15);
+const calk_1 = __importDefault(require("../src/calk"));
+describe("test calculateScore methods", () => {
+    let bowling;
+    beforeEach(() => {
+        bowling = new calk_1.default();
     });
-    it("should return 5 for add(2,3)", () => {
-        expect((0, calk_1.add)(2, 3)).toBe(5);
+    it("should return 15 for calculateScore('X|11')", () => {
+        const bowlingScore = bowling.calculateScore("X|11");
+        expect(bowlingScore).toBe(14);
     });
 });
